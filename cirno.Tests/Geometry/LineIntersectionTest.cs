03d6070fe6611dde2aa1dbd55f405ce118e7c5a5
20chan <2@0chan.dev>
2019-09-16 23:10:50 +0900

    Add LineSeg SegSeg TC

--- cirno.Tests/Geometry/LineIntersectionTest.cs
+++ cirno.Tests/Geometry/LineIntersectionTest.cs
@@ -2,7 +2,6 @@
 using cirno.Geometry;
 using static cirno.Tests.Geometry.GeometryTestHelper;
 
-
 namespace cirno.Tests.Geometry {
     [TestClass]
     public class LineIntersectionTest {
@@ -27,12 +26,30 @@ namespace cirno.Tests.Geometry {
 
         [TestMethod]
         public void TestLineSeg() {
+            AssertIntersect(L(0, 0, 1, 1), S(0, 1, 1, 0), V(0.5f, 0.5f));
+            AssertIntersect(L(0, 0, 1, 0), S(0, 0, 0, 1), V(0f, 0f));
+            AssertIntersect(L(1, 0, 2, 0), S(0, 0, 0, 1), V(0f, 0f));
+        }
 
+        [TestMethod]
+        public void TestLineSegNo() {
+            AssertIntersect(L(0, 0, 1, 0), S(0, 1, 0, 2));
+            AssertIntersect(L(1, 0, 2, 0), S(0, 1, 0, 2));
+            AssertIntersect(L(0, 0, 1, 0), S(0, 1, 1, 1));
+            AssertIntersect(L(0, 0, 0, 1), S(1, 0, 1, 1));
         }
 
         [TestMethod]
         public void TestSegSeg() {
+            AssertIntersect(S(0, 0, 1, 1), S(0, 1, 1, 0), V(0.5f, 0.5f));
+            AssertIntersect(S(0, 0, 1, 1), S(0, 2, 1, 1), V(1, 1));
+            AssertIntersect(S(0, 0, 1, 0), S(0, 0, 0, 1), V(0f, 0f));
+        }
 
+        [TestMethod]
+        public void TestSegSegNo() {
+            AssertIntersect(S(0, 0, 1, 1), S(0, 2, 0.9f, 0.9f));
+            AssertIntersect(L(1, 0, 2, 0), S(0, 0, 0, 1));
         }
     }
 }
